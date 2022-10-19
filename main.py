667dfb242533105648ae2ffd3b64429afc9c1b38
azpantiс <petrpetrovich9828@gmail.com>
2022-10-19 11:08:04 +1000

    Change Socket to API

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from flask import Flask, render_template,request
 from flask_socketio import SocketIO , send , join_room , leave_room
+from flask_restful import Resource , Api 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+api = Api(app)
 
-LED_SID = None
+Current_Colror : str = "#ffffff"
 
-@socketio.on('message')
-def handle_message(data):
+class Color(Resource):
     
-    send(data , broadcast=True)
+    def get(self):
+        return {"color" : Current_Colror}
 
-@socketio.on('join')
-def on_join(data):
-    LED_SID  = request.sid
+api.add_resource(Color , '/api/color')
 
-@app.route("/")
+
+@app.route("/" , methods = ["POST" , "GET"])
 def index():
-    return render_template("index.html")
+    
+    global Current_Colror
+    
+    if request.method == "POST":
+        Current_Colror = request.form["color"]
+        
+        
+    return render_template("index.html" , color = Current_Colror)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run(debug=True)
